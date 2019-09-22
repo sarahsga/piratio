8080ff70d2db9ef9df23493dbbaa86b263e802ff
sarahsga <sarahsga4@gmail.com>
2019-09-22 08:53:53 +0500

    Updates

---
+++ precache-manifest.ea64ecc84eddffe718f241018a5d79d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2098b6c7af65074fd5f7a400939d27e9",
+    "url": "/piratio/index.html"
+  },
+  {
+    "revision": "774e7dbf894df318cee9",
+    "url": "/piratio/static/css/main.e1421abf.chunk.css"
+  },
+  {
+    "revision": "5a7a7b4f9d50a32cbf38",
+    "url": "/piratio/static/js/2.a18f6c80.chunk.js"
+  },
+  {
+    "revision": "774e7dbf894df318cee9",
+    "url": "/piratio/static/js/main.8d41d939.chunk.js"
+  },
+  {
+    "revision": "d9f19e0e1013e4dea0fc",
+    "url": "/piratio/static/js/runtime~main.0d9690fe.js"
+  }
+]);
